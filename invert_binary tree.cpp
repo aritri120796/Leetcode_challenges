58668ce4a9acc644cb7cd8e9162c39ba98b4e4b4
aritri120796 <aritrichakraborty@gmail.com>
2020-06-04 11:05:17 +0530

    Update invert_binary tree.cpp

--- invert_binary tree.cpp
+++ invert_binary tree.cpp
@@ -1,14 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#include <iostream>
+using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
@@ -24,4 +25,29 @@ public:
         invertTree( root->right);}
         return root;
     }
+    TreeNode* preorder(TreeNode* root){
+    	
+    	if(root==NULL){
+    		cout<<"NULL"<<endl;
+    
+    	return root;}
+    	preorder(root->left);
+    	cout<<root->val<<endl;
+    	preorder(root->right);
+    	return root;
+    }
 };
+
+int main() {
+	TreeNode* t=new TreeNode(1,NULL,NULL);
+	
+	
+	TreeNode* r=new TreeNode(2,NULL,NULL);
+	
+	TreeNode* root=new TreeNode(1,t,r);
+
+	Solution* s=new Solution();
+	root=s->invertTree(root);
+	s->preorder(root);
+	return 0;
+}
